2df004e5e40db8c5b271a9b974b178b48fc1fb50
ael493 <54331766+abma493@users.noreply.github.com>
2023-01-23 20:15:33 +0000

    Initial commit

---
+++ AnagramSolver.java
@@ -0,0 +1,123 @@
+/* 
+ *  AnagramSolver class solves all possible anagrams in a given word or phrase.
+ *  
+ *  Based on a program by Stuart Reges, written by Abraham Martinez.
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+import java.util.TreeMap;
+
+public class AnagramSolver {
+
+  private TreeMap<String, LetterInventory> wordsInventory;
+  private TreeMap<String, LetterInventory> filtered;
+  private LetterInventory wordPhrase;
+
+  /*
+   * pre: list != null
+   * 
+   * @param list Contains the words to form anagrams from.
+   */
+  public AnagramSolver(Set<String> dictionary) {
+    if (dictionary == null) {
+      throw new IllegalArgumentException("Dictionary is empty.");
+    }
+    wordsInventory = new TreeMap<>();
+    Iterator<String> itr = dictionary.iterator();
+    while (itr.hasNext()) {
+      String key = itr.next();
+      wordsInventory.put(key, new LetterInventory(key));
+    }
+  }
+
+  /*
+   * pre: maxWords >= 0, s != null, s contains at least one
+   * English letter.
+   * Return a list of anagrams that can be formed from s with
+   * no more than maxWords, unless maxWords is 0 in which case
+   * there is no limit on the number of words in the anagram
+   */
+  public List<List<String>> getAnagrams(String s, int maxWords) {
+    if (maxWords < 0 || s == null || !(s.matches(".*[a-zA-Z]+.*"))) {
+      throw new IllegalArgumentException("Invalid word/phrase or max");
+    }
+    wordPhrase = new LetterInventory(s);
+    filterDictionary(wordPhrase);
+    Set<Stack<String>> getAnagrams = new HashSet<>();
+    ArrayList<List<String>> results = new ArrayList<>();
+    getAnagrams(new LetterInventory(""), new Stack<>(), getAnagrams, maxWords);
+    results.addAll(getAnagrams);
+    Collections.sort(results, new AnagramComparator());
+    return results;
+  }
+
+  // Helper method for getAnagrams(), employs recursion to find anagrams.
+  private void getAnagrams(LetterInventory candidate, Stack<String> currAnagram, Set<Stack<String>> results, int max) {
+    if (candidate.equals(wordPhrase) && currAnagram.size() <= max) {
+      System.out.println(candidate.toString() + "\n");
+      Stack<String> safeCopy = deepCopy(currAnagram);
+      Collections.sort(safeCopy);
+      results.add(safeCopy);
+    } else {
+      for (String word : filtered.keySet()) {
+        LetterInventory possible = candidate.add(filtered.get(word));
+        boolean isCandidate = isCandidate(possible);
+        if (isCandidate) {
+          // System.out.println(filtered.get(word));
+          currAnagram.push(word);
+          getAnagrams(possible, currAnagram, results, max);
+          currAnagram.pop();
+        }
+      }
+    }
+  }
+
+  // Helper method for recursive getAnagrams()
+  private boolean isCandidate(LetterInventory possible) {
+    return wordPhrase.subtract(possible) != null && wordPhrase.subtract(possible).size() != 1;
+  }
+
+  // Helper method for recursive method. Creates a deep copy for the current
+  // Anagram to prevent data from being overwritten.
+  private Stack<String> deepCopy(Stack<String> currAnagram) {
+    Stack<String> result = new Stack<>();
+    for (String elem : currAnagram) {
+      result.push(elem);
+    }
+    return result;
+  }
+
+  // filters the dictionary to allow only words with relevant letters from
+  // wordPhrase.
+  private void filterDictionary(LetterInventory wordPhrase) {
+    filtered = new TreeMap<>();
+    for (String word : wordsInventory.keySet()) {
+      LetterInventory itsInventory = wordsInventory.get(word);
+      if (wordPhrase.subtract(itsInventory) != null) {
+        filtered.put(word, itsInventory);
+      }
+    }
+  }
+
+  // An inner class to allow comparison for List of Lists.
+  private static class AnagramComparator implements Comparator<List<String>> {
+    @Override
+    public int compare(List<String> a1, List<String> a2) {
+      if (a1.size() < a2.size()) {
+        return -1;
+      } else if (a1.size() > a2.size()) {
+        return 1;
+      } else {
+        return a1.get(0).compareTo(a2.get(0));
+      }
+    }
+  }
+
+}
---
+++ LetterInventory.java
@@ -0,0 +1,144 @@
+/*
+ * 
+ * LetterInventory class for the Anagram Assignment. This class stores the number
+ * of times each English letter 'a' through 'z' occur in a word or phrase.
+ * 
+ * Written by Abraham Martinez 
+ */
+public class LetterInventory {
+
+  private int[] lettersFreq;
+  private static final int ALPHABET = 26;
+  private String phrase;
+
+  public LetterInventory(String phrase) {
+    if (phrase == null) {
+      throw new IllegalArgumentException("Word/Phrase is null");
+    }
+    lettersFreq = new int[ALPHABET];
+    String lowerCase = phrase.toLowerCase();
+    this.phrase = lowerCase;
+    for (int ch = 0; ch < lowerCase.length(); ch++) {
+      char letter = lowerCase.charAt(ch);
+      if (letter >= 'a' && letter <= 'z') {
+        int index = (letter - 'a');
+        lettersFreq[index]++;
+      }
+    }
+  }
+
+  /*
+   * pre: char must be an English letter (A-Z)/(a-z)
+   * post: get the frequency number of a valid letter.
+   */
+  public int get(char letter) {
+    if ((letter < 'a' && letter > 'z') || (letter < 'A' && letter > 'Z')) {
+      throw new IllegalArgumentException("Letter not in the English Alphabet");
+    }
+    char base = (letter < 'a') ? 'A' : 'a';
+    int index = (letter - base);
+    return lettersFreq[index];
+  }
+
+  /*
+   * Returns the total number of letters in this LetterInventory.
+   */
+  public int size() {
+    int size = 0;
+    for (int i = 0; i < lettersFreq.length; i++) {
+      size += lettersFreq[i];
+    }
+    return size;
+  }
+
+  /*
+   * Checks if the size of this inventory is 0.
+   */
+  public boolean isEmpty() {
+    int size = this.size();
+    return size == 0;
+  }
+
+  /*
+   * Returns a string representation of the Letter inventory array.
+   * It is ordered alphabetically, thus the letters return sorted.
+   */
+  public String toString() {
+    String result = "";
+    int index = 0;
+    while (index < lettersFreq.length) {
+      char letter = (char) (index + 'a');
+      int times = 0;
+      while (times < lettersFreq[index]) {
+        result += letter;
+        times++;
+      }
+      index++;
+    }
+    return result;
+  }
+
+  /*
+   * param(s): Another LetterInventory Object
+   * pre: other != null
+   * post: new LetterInventory created by addingthe frequencies from the calling
+   * object (this) and (other) object.
+   */
+  public LetterInventory add(LetterInventory other) {
+    if (other == null) {
+      throw new IllegalArgumentException("LetterInventory object is null.");
+    }
+    LetterInventory result = new LetterInventory(this.phrase + other.phrase);
+    return result;
+  }
+
+  /*
+   * param(s): Another LetterInventory Object
+   * pre: other != null
+   * post: new LetterInventory Obj. created by subtracting the letter frequencies
+   * of the explicit parameter from the calling object's letter frequencies.
+   * 
+   */
+  public LetterInventory subtract(LetterInventory other) {
+    if (other == null) {
+      throw new IllegalArgumentException("LetterInventory object is null.");
+    }
+    LetterInventory result = new LetterInventory(this.phrase);
+    for (int i = 0; i < other.phrase.length(); i++) {
+      char let = other.phrase.charAt(i);
+      if (let != ' ' && let != '-' && let != '"') {
+        int index = (int) (let - 'a');
+        if (result.phrase.contains("" + let)) {
+          result.lettersFreq[index]--;
+          if (result.lettersFreq[index] < 0) {
+            return null;
+          }
+        } else if (result.lettersFreq[index] == 0) { // doesn't contain letter
+          return null;
+        }
+      }
+    }
+    return result;
+  }
+
+  /*
+   * Determines if this LinkedList is equal to other.
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof LetterInventory) || obj == null) {
+      return false;
+    } else if (obj == this) {
+      return true;
+    } else {
+      LetterInventory other = (LetterInventory) obj;
+      for (int freq = 0; freq < lettersFreq.length; freq++) {
+        if (this.lettersFreq[freq] != other.lettersFreq[freq]) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+
+}
